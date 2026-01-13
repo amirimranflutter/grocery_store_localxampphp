0e6f6a52a739976e27c77d8cfdda06ca0465b27c
Umar <umern9947@gmail.com>
2026-01-13 16:01:17 +0500

    bug fixes

--- xampp/htdocs/grocery_api/products/add_product.php
+++ xampp/htdocs/grocery_api/products/add_product.php
@@ -4,13 +4,11 @@ include '../config/db_connection.php';
 $name = $_POST['p_name'];
 $price = $_POST['price'];
 $stock = $_POST['stock'];
-$quantity = $_POST['quantity'];
 $cat_id = $_POST['cat_id'];
-$date = $_POST['date_added'];
+$date = $_POST['date'];
 
-// Professional Tip: Use Prepared Statements to prevent SQL Injection
-$stmt = $conn->prepare("INSERT INTO products (p_name, price, stock, cat_id,date_added) VALUES (?, ?, ?, ?)");
-$stmt->bind_param("sddi", $name, $price, $stock, $cat_id,$date);
+$stmt = $conn->prepare("INSERT INTO products (p_name, price, stock, cat_id, date) VALUES (?, ?, ?, ?, ?)");
+$stmt->bind_param("sdsis", $name, $price, $stock, $cat_id, $date);
 
 if($stmt->execute()) {
     echo json_encode(["status" => "success"]);
